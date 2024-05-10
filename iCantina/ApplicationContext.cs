7d8d6a0dc8a66734848eda9bff19347e26ab7b51
Rodrigo Miguel Neto Jerónimo <jeronimorodrigo04@gmail.com>
2024-05-10 18:03:22 +0100

    começo de projeto

---
+++ iCantina/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iCantina
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Fatura> Faturas { get; set; }
+        public DbSet<Extra> Extras { get; set; }
+        public DbSet<Utilizador> Utilizadores { get; set; }
+        public DbSet<Filme> Filmes { get; set; }
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<Prato> Pratos { get; set; }
+        public DbSet<Reserva> Reservas { get; set; }
+    }
+}
